b6bef6445049cfd7f4b576bdff009fe21b8ce803
Segun <segunvictor55@gmail.com>
2020-04-15 14:16:36 +0100

    for task3

--- intern.js
+++
@@ -1,7 +0,0 @@
-db.interns.insert({
-data:{
-    name: 'Adetunji_Segun',
-    email: 'segunvictor55@yahoo.com',
-    address: 'Back End, Start.NG.'},
-});
-console.log("interns collection created");
